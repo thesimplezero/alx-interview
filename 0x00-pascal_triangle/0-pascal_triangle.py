c04e0e3ff8a8a7baa4ccf365fdef457fdcb82318
thesimplezero <hi@thesimplezero.io>
2024-01-04 19:23:25 +0300

    Refactor pascal_triangle to meet code requirements

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,11 +14,18 @@ def pascal_triangle(n):
 
     if n <= 0:
         return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    
+    triangle = [[1], [1, 1]]
 
-    triangle = [[1]]
-    for _ in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1] + [a + b for a, b in zip(prev_row, prev_row[1:])] + [1]
-        triangle.append(new_row)
+    for i in range(2, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
 
     return triangle
