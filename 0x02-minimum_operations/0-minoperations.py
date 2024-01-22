18598a1c9cf403537eaa1523dabb60ef45c8f98c
thesimplezero <hi@thesimplezero.io>
2024-01-22 03:21:39 +0300

    fix errors

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """
 Module to calculate the fewest number of operations needed to
 result in exactly n 'H' characters in a text file. The two
@@ -6,18 +7,13 @@ allowed operations are Copy All and Paste.
 """
 
 
-def min_operations(n):
-    """
-    Calculate Minimum Operations.
-    """
-    if not isinstance(n, int) or n <= 1:
+def minOperations(n):
+    """ minOperations function """
+    if n <= 1:
         return 0
-    divisor = 1
-    i = n - 1
-    while i > 0:
-        if n % i == 0:
-            divisor = i if i != 0 else 1
-            break
-        i -= 1
-    result = int((min_operations(divisor) if divisor != 1 else 0) + n / divisor)
-    return result
+    else:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                return minOperations(int(n / i)) + i
+
+    return 0
